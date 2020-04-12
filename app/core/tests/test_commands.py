de0f3631e52c3ce2ac45a2e5caf5d13dd301f121
Pawel Tucholski <sorryej@gmial.com>
2020-04-12 22:15:28 +0200

    Added 'using=self._db' to save user in custom model(models.py)

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -7,10 +7,11 @@ from django.test import TestCase
 class CommandTest(TestCase):
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
-            get_item.return_value = True
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # gi = get_item
+            gi.return_value = True
             call_command('wait_for_db')
-            self.assertEqual(get_item.call_count, 1)
+            self.assertEqual(gi.call_count, 1)
 
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
